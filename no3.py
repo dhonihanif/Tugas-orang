9a57a87f9dd6f59c0d149f79716551b198cfb09d
Dhoni Hanif Supriyadi <66652598+dhonihanif@users.noreply.github.com>
2021-12-07 21:45:38 +0700

    Delete no3.py

--- no3.py
+++
@@ -1,9 +0,0 @@
-#no 3
-
-x = int(input("Masukkan sebuah nilai : "))
-if(x%4==0):
-    print("Nilai {} adalah kelipatan 4".format(x))
-
-else:
-    print("Maaf Nilai {} bukan kelipatan 4".format(x))
-    
